5ac2c1eb334415073c0679f4289c9616b48e1c71
mahtokundan5337-blip <mahtokundan5337@gmail.com>
2026-02-11 22:01:42 +0530

    Update a=10.py

--- a=10.py
+++ a=10.py
@@ -16,10 +16,24 @@ if num % 2 == 0:
     print("Even")
 elsr:
     print("Odd")  """
-a=10
+'''a=10
 b=2
 print(a+b)
 print(a-b)
 print(a*b)
 print(a/b)
-print(a%b)
+print(a%b)'''
+#list
+'''n=[10,20,30,40,50,]
+print(n)
+print ("first:"n=[0])
+
+print ("last"n=[-1])'''
+'''n=[1,2,3]
+n.append(4)
+print(n)'''
+n=[1,2,3,4,5,6 ,7,8,9,10,11,12,13,14,15]
+for n in n:
+    if n %2==1:#odd number
+
+     print(n)
