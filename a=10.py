38075a54d893bfae57e6a885542c5be33c4c38bb
mahtokundan5337-blip <mahtokundan5337@gmail.com>
2026-02-12 15:19:51 +0530

    Update a=10.py

--- a=10.py
+++ a=10.py
@@ -83,5 +83,44 @@ print("sum=",total)'''
 for i in range(1,11):
     print(n,"x",i,"=",n*i)'''
 
-for i in range(15,0,-1):
-    print(i)
+'''for i in range(15,0,-1):
+    print(i)'''
+
+'''for i in range (15,0,-1):#reverse 
+    print("*"* i)'''
+
+'''n=int(input("enter the value"))
+c=0
+for i in range(1,n+1):
+    if n%i==0:
+        c+=1
+if c==2:
+    print("prime")
+else:
+    print("not prime")'''
+
+n=10
+a=0
+b=1
+'''for i in range(n):
+    print(a)
+    c=a+b
+    a=b
+    b=c'''
+
+'''n=54
+c=0
+
+while n>0:
+    n//=10
+    c+=1
+print("digits",c)'''
+n=123
+rev=0
+
+while n> 0:
+ dight=n%10
+ rev=rev*10+dight
+ n//=10
+
+print("rev",rev)
