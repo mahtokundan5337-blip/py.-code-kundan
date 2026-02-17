8ae5c8c466dac1331a29d23d99bba69cdd21f814
mahtokundan5337-blip <mahtokundan5337@gmail.com>
2026-02-17 20:17:00 +0530

    Update a=10.py

--- a=10.py
+++ a=10.py
@@ -1,107 +1,13 @@
-'''a=10
-b=20
-print(a+b) '''
-'''a=10
-b=3.14
-c="python"
-d=True
+n=int(input("enter the number"))
+if n >1:
+    for i in range (2,n):
+        if n%i==0:
+            print("non prime number" )
 
-print(type(a))
-print(type(b))
-print(type(c))
-print(type(d))'''
-"""num = int(input("Enter number:"))
-
-if num % 2 == 0:
-    print("Even")
-elsr:
-    print("Odd")  """
-'''a=10
-b=2
-print(a+b)
-print(a-b)
-print(a*b)
-print(a/b)
-print(a%b)'''
-#list
-'''n=[10,20,30,40,50,]
-print(n)
-print ("first:"n=[0])
-
-print ("last"n=[-1])'''
-'''n=[1,2,3]
-n.append(4)
-print(n)'''
-'''n=[1,2,3,4,5,6 ,7,8,9,10,11,12,13,14,15]
-for n in n:
-    if n %2==1:#odd number
-
-     print(n)'''
-
-#dictonary
-'''student={"name","kundan","marks"}
-print(student)
+    else:
+        print("prime number") 
+else:
+    print("not prime number")
 
 
-#dict..... loop
-if"marks" in Student:
-    print("marks exist")'''
-
-
-#set
-'''S={1,2,3,4}
-print(S)
-S.add(5)
-print(S)
-S.remove(2)
-print(S)'''
-''''#a={1,2,3,}
-b={3,4,5,}
-print(a|b)#'''
-#tuple
-'''t=(10,20,30)
-print(t)
-
-l=list(t)
-print(l)'''
-
-'''t=(1,2,3,2,2)
-print(t.count(2))'''
-
-"""for i in range(1,11):
-    print(i)"""
-
-'''for i in range (1,11):
-    if i % 2==1:
-        print(i)'''
-'''total = 0
-for i in range (1,11):
-    total+=i
-print("sum=",total)'''
-
-'''n=5
-for i in range(1,11):
-    print(n,"x",i,"=",n*i)'''
-
-'''for i in range(15,0,-1):
-    print(i)'''
-
-for i in range (1,5):#reverse 
-    print("*"*i)
-
-
-'''for i in range(1,6):
-    for j in range(i):
-        print (i,emd="")
-    print(str(i)*i)'''
-
-
-
-r=4
-for i in range(1,r+1):
-    n=i
-    for j in range(i):
-        print(n,end="")
-        n+=1
-        print()
 
